a1091bd31958aee4d3e923baabd4ff48efbbe1e9
wangerxiao66 <784665813@qq.com>
2020-06-14 15:16:17 +0800

    ...

--- src/main/java/com/remango/blog/BlogApplication.java
+++ src/main/java/com/remango/blog/BlogApplication.java
@@ -1,12 +1,13 @@
 package com.remango.blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication extends SpringBootServletInitializer {
+public class BlogApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
@@ -17,4 +18,8 @@ public class BlogApplication extends SpringBootServletInitializer {
 		return builder.sources(BlogApplication.class);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("服务启动成功");
+	}
 }
--- src/main/java/com/remango/blog/controller/TagsController.java
+++ src/main/java/com/remango/blog/controller/TagsController.java
@@ -1,5 +1,6 @@
 package com.remango.blog.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.remango.blog.aop.ControllerLog;
 import com.remango.blog.entity.PageList;
 import com.remango.blog.entity.Tags;
@@ -7,10 +8,9 @@ import com.remango.blog.service.TagsService;
 import com.remango.blog.vo.ResultBean;
 import com.remango.blog.vo.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -24,8 +24,10 @@ public class TagsController {
     @Autowired
     private TagsService tagsService;
     @ResponseBody
-    @GetMapping("/getTags")
-    public ResultBean getTags() {
+    @RequestMapping("/getTags")
+    public ResultBean getTags(String name) {
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println(JSON.toJSONString(((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getParameterMap()));
         PageList<Tags> PageLogList = tagsService.queryTagsList();
         return new ResultBean(PageLogList);
     }
